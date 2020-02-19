e4a5fdfce5fd615ea04320b45be70d167e2ea0e6
Suyash Sharma <suyash1706@gmail.com>
2020-02-19 17:34:54 -0500

    extra files

---
+++ client.py
@@ -0,0 +1,51 @@
+
+import threading
+import time
+import random
+import socket as mysoc
+
+
+# client task - given
+
+
+def client():
+    try:
+        cs = mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
+        print("[C]: Client socket created")
+    except mysoc.error as err:
+        print('{} \n'.format("socket open error ", err))
+
+# Define the port on which you want to connect to the server
+    port = 50007
+    sa_sameas_myaddr = mysoc.gethostbyname(mysoc.gethostname())
+# connect to the server on local machine
+    server_binding = (sa_sameas_myaddr, port)
+    cs.connect(server_binding)
+
+# Opening the file in which string data is present which is to be sent to the server.
+    file = open("HW1test.txt", "r")
+    for line in file:
+        print(line)
+        cs.sendall(bytes(line, 'utf-8'))
+    file.close()
+
+# Opening the file in which data recieved back from the server has to be written in form of ASCII values.
+    writeFile = open("HW1out.txt", "w")
+
+    while True:
+        data_from_server = cs.recv(1024)
+        strReceived = data_from_server.decode('utf-8')
+        print("[C]: Data received from server::  ",
+              strReceived)
+
+        writeFile.write(strReceived + "\n")
+
+    writeFile.close()
+
+
+t2 = threading.Thread(name='client', target=client)
+t2.start()
+
+input("Hit ENTER  to exit")
+
+exit()
--- rs.py
+++ rs.py
@@ -4,3 +4,65 @@ class ListNode(object):
         self.hostname = hostname
         self.ipAddress = ipAddress
         self.flag = flag
+
+
+
+import threading
+import time
+import random
+import socket as mysoc
+
+# server task - given
+
+
+def server():
+    stringManipulation("Hello")
+    try:
+        ss = mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
+        print("[S]: Server socket created")
+    except mysoc.error as err:
+        print('{} \n'.format("socket open error ", err))
+    server_binding = ('', 50007)
+    ss.bind(server_binding)
+    ss.listen(1)
+    host = mysoc.gethostname()
+    print("[S]: Server host name is: ", host)
+    localhost_ip = (mysoc.gethostbyname(host))
+    print("[S]: Server IP address is  ", localhost_ip)
+    csockid, addr = ss.accept()
+    print("[S]: Got a connection request from a client at", addr)
+
+    # While the input data exists, keep on reading the data. When there is no input, it is because we have reached the end of file.
+    while True:
+        try:
+            input = csockid.recv(10240)
+            print(input)
+            data = input.decode('utf-8')
+            print("Client sent: " + data)
+            msg = stringManipulation(data)
+            csockid.send(msg.encode('utf-8'))
+        except mysoc.error:
+            print("END OF FILE")
+            break
+    ss.close()
+    exit()
+
+# stringManipulation takes in an input String as a parameter and then returns the string converted to ASCII value, character by character.
+
+
+def stringManipulation(inputString):
+    result = ""
+    for element in inputString:
+        if len(result) == 0:
+            result = result + str(ord(element))
+        else:
+            result = result + "_" + str(ord(element))
+    return result
+
+
+t1 = threading.Thread(name='server', target=server)
+t1.start()
+
+input("Hit ENTER  to exit")
+
+exit()
